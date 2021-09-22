42642f07ea76aa8db6c7cbc26c2b27da5630228d
Mümtaz Taha KÜÇÜK <86522804+Taha2341@users.noreply.github.com>
2021-09-22 16:29:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mylıst<string> isimler = new Mylıst<string>();
+            isimler.Add("engin");
+            Console.WriteLine(isimler.length);
+            isimler.Add("kerem");
+            Console.WriteLine(isimler.length);
+
+
+
+        }
+    }
+}
